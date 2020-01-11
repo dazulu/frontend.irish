fb3131b0210e3712ad15232301506556782b4808
dazulu <definitive@outlook.com>
2020-01-11 21:40:01 +0100

    feat(home): adds placeholder text

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,7 +3,19 @@ import React from "react"
 function home() {
   return (
     <div className='page'>
-      <h1 className='title'>Home</h1>
+      <h1 className='title'>Who Knows</h1>
+      <p>
+        A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my
+        whole heart.
+      </p>
+
+      <p>I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.</p>
+
+      <p>
+        I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents.
+        I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist
+        than now. When, while the lovely valley teems with
+      </p>
     </div>
   )
 }
